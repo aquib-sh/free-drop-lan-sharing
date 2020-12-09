509977476ca128bcbd6711420f200fe8d4a44831
Aquib <shaikhaquib394@gmail.com>
2020-12-09 18:42:42 +0530

    Added functions for CRUD

--- freedrop/resources/db_helper.py
+++ freedrop/resources/db_helper.py
@@ -1,52 +1,72 @@
 """ AUTHOR: SHAIKH AQUIB
     THIS WILL SERVER AS A SET OF HELPFUL FUNCTIONS TO MAKE DATABASE OPERATIONS EASIR IN SERVER
 """
+import sys
+
 
 class DBHelper:
 
     def __init__(self, cursor):
         self.cursor = cursor
 
-    # Fetches the cursor and returns list
     def fetch_list(self):
+        """ Fetches the cursor and returns list. """
         return [item[0] for item in self.cursor.fetchall()]
 
-    # Checks if path already exists in the table
     def path_already_exists(self, path_name, table_name):
+        """ Checks if path already exists in the table. """
         self.cursor.execute('SELECT PATH FROM {} WHERE PATH="{}"'.format(table_name, path_name))
         if len(self.fetch_list()) == 0:
             return False
         return True
 
-    # Returns the list of path  names from the searched path
     def get_path(self, path_name, table_name):
-        self.cursor.execute('SELECT PATH FROM {} WHERE PATH="{}"'.format(table_name, path_name))
+        """ Returns the list of path  names from the searched path. """
+        self.cursor.execute('SELECT PATH FROM {} WHERE FILENAME="{}"'.format(table_name, path_name))
         return self.fetch_list()
 
-    # Returns file format
     def get_format(self, path_name, table_name):
+        """ Returns file format. """
+        if sys.platform == "win32":
+            path_name = path_name.replace("\\", r"\\")
         self.cursor.execute('SELECT format FROM {} WHERE path="{}"'.format(table_name, path_name))
         return self.fetch_list()
 
-    # Returns the list of path names from the searched path with regular expressions
     def search_path(self, path_name, table_name):
+        """ Returns the list of path names from the searched path with regular expressions. """
         self.cursor.execute('SELECT PATH FROM {} WHERE PATH REGEXP "{}"'.format(table_name, path_name))
         return self.fetch_list()
 
-    # Returns a list of an entire column from table present on server database
     def get_full_list(self, column_name, table_name):
+        """ Returns a list of an entire column from table present on server database """
         self.cursor.execute('SELECT {} FROM {}'.format(column_name, table_name))
         return self.fetch_list()
 
-    # Insert into table
-    # creation date will always be the current one
     def insert_value(self, table, path, file_format):
-        filename = path.replace("images/","")
+        """ 
+            Inserts record into the database 
+            by formatting the path according to 
+            the current operating system.
+        """
+        if sys.platform == "win32":
+            filename = path.split("\\")[-1]
+            path     = path.replace("\\", r"\\")
+        elif sys.platform == "linux":
+            filename = path.split("/")[-1]
+
         self.cursor.execute("""INSERT INTO {0} (path, filename, format, creation) 
             VALUES ("{1}", "{2}", "{3}", NOW())
             """.format(table, path, filename, file_format))
 
-    # Delete records from table
     def delete_value(self, table, filename):
+        """ Delete records from table. """
         self.cursor.execute('DELETE FROM {} WHERE filename="{}"'.format(table, filename))
-        
+
+    def update_value(self, table, column, current_value, new_value):
+        """ Delete records from table. """
+        self.cursor.execute('UPDATE {tab} SET {col}="{new_val}" WHERE {col}="{cur_val}"'.format(
+            tab     = table, 
+            col     = column, 
+            cur_val = current_value, 
+            new_val = new_value)
+            )
--- freedrop/resources/util.py
+++ freedrop/resources/util.py
@@ -1,4 +1,5 @@
 import datetime
+import sys
 
 # Creates new table structure 
 def set_table_structure(table_name):
@@ -14,8 +15,15 @@ def set_table_structure(table_name):
 
 # Returns current date for naming the file
 def get_formatted_date():
-        date_obj = datetime.datetime.now()
-        date = date_obj.date()
-        # Current formatted date
-        formatted_date = str(date.day) + "-" + str(date.month) + "-" + str(date.year)
-        return formatted_date
+    date_obj = datetime.datetime.now()
+    date = date_obj.date()
+    # Current formatted date
+    formatted_date = str(date.day) + "-" + str(date.month) + "-" + str(date.year)
+    return formatted_date
+
+# Returns file name for windows
+def format_path(file_name):
+    to_check = file_name
+    if sys.platform == "win32":
+        to_check = to_check.replace("\\", r"\\\\")
+    return to_check
